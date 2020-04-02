efcde2360de1bc027c23765f03f0b880157a9137
Paniuta.p <paniuta.p@gmail.com>
2020-04-02 21:19:48 +0300

    page game

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-function poiskChisla () {
 
+var prNum, tempOut;
+        prNum = Math.floor((Math.random() * 10) + 1);
+  
+        console.log (prNum);
+    // tempOut = document.getElementById ('temp-out');
+    // tempOut.innerHTML == prNum;
+
+function f1 () {
+    var num,  out;
+     num = document.getElementById ('my-num').value;
+     out = document.getElementById ('out');
+     
+
+     if (num == prNum) {
+         out.innerHTML = 'Вы угодали!!!';
+     }
+     else if (num > prNum) {
+         out.innerHTML = 'Вы введи число больше чем нужно';
+     }
+     else if (num < prNum) {
+         out.innerHTML = 'Вы ввели число меньше чем нужно';
+     }
+     else  {
+         out.innerHTML ='Введите число от 1 до 10';
+     }
 }
